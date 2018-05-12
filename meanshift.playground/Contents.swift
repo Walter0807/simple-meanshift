f9af6a74d2ab7aba3197490c99087d5908995985
Walter0807 <walterzhu@pku.edu.cn>
2018-05-12 10:39:57 +0800

    somehow proceed

--- meanshift.playground/Contents.swift
+++ meanshift.playground/Contents.swift
@@ -3,31 +3,67 @@
 import UIKit
 
 let imgsrc = RGBAImage(image: UIImage(named: "sample")!)!
+let width = Int(imgsrc.width)
+let height = Int(imgsrc.height)
+let MIN_DISTANCE = 0.00001
+let MIN_DISTANCE_GROUP = 0.1
+
+var allPoints = [Point]()
+var easyFind = [[Int]]()
+var cateCenter = [Int]()
+var cateCounter = 0
+
+for i in 0..<height {
+    for j in 0..<width {
+        let index = i * width + j
+        let pixel = imgsrc.pixels[index]
+        allPoints += [Point(Double(pixel.R), Double(pixel.G), Double(pixel.B), i, j)]
+    }
+}
+
+//allPoints.shuffle()
+
+public func meanShift() {
+    var shiftingPointNumbers = allPoints.count
+    while shiftingPointNumbers > 0 {
+        for point in allPoints {
+            if !point.shifting {continue}
+            let pointShifted = pointShift(point)
+            dist = distEuchilid(point, pointShifted)
+            if  dist< MIN_DISTANCE {
+                point.shifting = false
+                shiftingPointNumbers -= 1
+            }
+            newPoint[point.x][point.y] = pointShifted
+        }
+        easyFind = [[]]
+        for (offset, point) in allPoints.enumerated() {
+            point = newPoint[point.x][point.y]
+            easyFind[Int(point.x)] += [offset]
+        }
+    }
+}
+
+public func groupPoints() {
+    for point in allPoints {
+        var newCate = true
+        for cc in cateCenter {
+            if distEuchilid(point, cc) < MIN_DISTANCE_GROUP {
+                newCate = false
+                point.cate = cc.cate
+            }
+            if newCate {
+                cateCounter += 1
+                point.cate = cateCounter
+                cateCenter += [point]
+            }
+        }
+    }
+}
 
 
-// Brightness
-ImageProcess.brightness(imgsrc, contrast: 0.8, brightness: 0.5).toUIImage()
 
-// Split
-let (R,G,B) = ImageProcess.splitRGB(imgsrc)
-R.toUIImage()
 
-// Sharpening
-let m1 = Array2D(cols:3, rows:3, elements:
-    [ 0.0/5.0, -1.0/5.0, 0.0/5.0,
-     -1.0/5.0,  9.0/5.0,-1.0/5.0,
-      0.0/5.0, -1.0/5.0, 0.0/5.0])
-ImageProcess.convolution(R.clone(), mask: m1).toUIImage()
-
-// Blur3x3
-let m2 = Array2D(cols: 3, rows: 3, elements:
-    [
-         1.0/9.0, 1.0/9.0, 1.0/9.0,
-         1.0/9.0, 1.0/9.0, 1.0/9.0,
-         1.0/9.0, 1.0/9.0, 1.0/9.0,
-    ]
-)
-ImageProcess.convolution(R.clone(), mask: m2).toUIImage()
 
 
 
---
+++ meanshift.playground/Sources/Point.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public struct Point {
+    public var R: Double
+    public var G: Double
+    public var B: Double
+    public var x: Int //0..<Height
+    public var y: Int //0..<Width
+    public var cate: Int = -1
+    public var shifting: Bool = true
+    
+    public init(_ r: Double, _ g: Double, _ b: Double, _ x: Int, _ y: Int) {
+        R = r
+        G = g
+        B = b
+        self.x = x
+        self.y = y
+    }
+    
+}
+
+extension Array {
+    public func randomItem() -> Element? {
+        if isEmpty { return nil }
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self[index]
+    }
+    
+    public func shuffle() -> Array {
+        var list = self
+        for index in 0..<list.count {
+            let newIndex = Int(arc4random_uniform(UInt32(list.count-index))) + index
+            if index != newIndex {
+                list.swapAt(index, newIndex)
+            }
+        }
+        return list
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
